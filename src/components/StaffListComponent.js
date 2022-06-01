8bfe9a390b2b35b94e9662ac60a3de5b3c98dc15
NguyenThanh <thnh.47@gmail.com>
2022-06-01 16:30:59 +0700

    second commit

---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class StaffList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default StaffList;
