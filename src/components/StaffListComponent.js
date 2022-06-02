d989439d588ff6ea11d081ee845583e5ff1d5be4
NguyenThanh <thnh.47@gmail.com>
2022-06-02 14:42:13 +0700

    third commit

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-
+import { Card, CardImg, CardImgOverlay, 
+    CardTitle } from 'reactstrap';
 
 class StaffList extends Component {
+    
+
     render() {
+        const staffs = this.props.
+        staffs.map((staff) => {
+            return (
+                <div>
+                    <Card key={staff.id}>
+                        <CardTitle>{staff.name}</CardTitle>
+                    </Card>
+                </div>
+            );
+
+        })
         return (
-            <div>
-                
-            </div>
-        );
+            <div>{staffs}</div>
+        )
     }
 }
 
