451bba2a96d9049f71bd67f2955dbf0be6087015
NguyenThanh <thnh.47@gmail.com>
2022-06-03 14:44:13 +0700

    six commit

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, 
+import { Card, CardBody, CardImg, CardImgOverlay, 
     CardTitle } from 'reactstrap';
+
 
 class StaffList extends Component {
     
+    constructor(props){
+        super(props);
+        this.state={
+            selectedStaff: null
+        }
+    }
+
+    onSelectedStaff(staff) {
+        this.setState({selectedStaff: staff})
+        console.log(this.state)
+    }
+    renderStaffDetail(staff) {
+        if (staff != null) {
+            return (
+                <Card>
+                    <CardBody>
+                        <CardTitle>{staff.name}</CardTitle>
+                    </CardBody>
+                </Card>
+            )
+        }
+        else {
+            return <div></div>
+        }
+    }
+
 
     render() {
-        const staffs = this.props.
-        staffs.map((staff) => {
+        const staffs = this.props
+        .staffs.map((staff) => {
             return (
                 <div className="col-12 col-md-6 col-lg-4 mt-2 mb-2">
-                    <Card key={staff.id}>
+                    <Card key={staff.id} onClick={() => this.onSelectedStaff(staff)} >
                         <CardTitle>{staff.name}</CardTitle>
                     </Card>
                 </div>
@@ -23,6 +50,9 @@ class StaffList extends Component {
                 {staffs}
                 </div>
                 <p>Bấm vào tên nhân viên để xem thông tin.</p>
+                <div>
+                    {this.renderStaffDetail(this.state.selectedStaff)}
+                </div>
             </div>
         )
     }
