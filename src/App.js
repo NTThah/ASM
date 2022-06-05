0e9ca35c6e1d48ad77ba6cc46ed4ecc47ea9ca57
NguyenThanh <thnh.47@gmail.com>
2022-06-05 22:45:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import StaffList from './components/StaffListComponent';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import { STAFFS } from './shared/staffs'
+import Main from './components/MainComponent';
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      staffs: STAFFS,
-      isDisplayForm: true
-    }
-  }
-  onCloseText = () => {
-    this.setState({
-      isDisplayForm: false,
-    });
-  };
+  
   render() {
-    const isDisplayForm = this.state.isDisplayForm;
-    var elm = isDisplayForm ? 'Bấm vào tên nhân viên để xem thông tin.' : "";
+    
     return (
       <div >
-        <Navbar dark color="primary" >
-            <div className="container">
-                <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-            </div>
-        </Navbar>
-            <StaffList staffs={this.state.staffs} onCloseText={this.onCloseText}/>
-            {elm}
+        <Main />
       </div>
     );
   }
---
+++ src/components/MainComponent.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+// import './App.css';
+import StaffList from './StaffListComponent';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { STAFFS } from '../shared/staffs'
+class Main extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      staffs: STAFFS,
+      isDisplayForm: true
+    }
+  }
+  onCloseText = () => {
+    this.setState({
+      isDisplayForm: false,
+    });
+  };
+  render() {
+    const isDisplayForm = this.state.isDisplayForm;
+    var elm = isDisplayForm ? 'Bấm vào tên nhân viên để xem thông tin.' : "";
+    return (
+      <div >
+        <Navbar dark color="primary" >
+            <div className="container">
+                <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+            </div>
+        </Navbar>
+            <StaffList staffs={this.state.staffs} onCloseText={this.onCloseText}/>
+            {elm}
+      </div>
+    );
+  }
+}
+
+export default Main;
