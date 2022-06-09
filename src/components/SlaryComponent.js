3e160399917ff857b698fb6735c66a6ee4b1d79e
NguyenThanh <thnh.47@gmail.com>
2022-06-09 22:55:28 +0700

    eight commit

--- src/components/SlaryComponent.js
+++ src/components/SlaryComponent.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { Card, CardBody, CardText } from "reactstrap";
+import { Card, CardBody, CardImg, CardText } from "reactstrap";
 
 const RenderSlary = ({slary}) => {
     return (
         <Card>
             <CardBody>
-                <CardText>{slary.name}</CardText>
+                <Card className="row  flex-row">
+                    <CardImg src={slary.image} className="col-6"/>
+                    <h6 className="col-6 text-center">{slary.name}</h6>
+                </Card>
+                <CardText>Mã nhân viên: {slary.id}</CardText>
+                        <CardText>Hệ số lương: {slary.salaryScale}</CardText>
+                        <CardText>Số ngày làm thêm: {slary.overTime} </CardText>
+                        <CardText className="bg-info pl-3 pt-2 pb-2 m-1">
+                            Lương: {(slary.salaryScale * 3000000 + slary.overTime * 200000).toFixed(0)}
+                        </CardText>
             </CardBody>
         </Card>
     )
@@ -13,7 +22,7 @@ const RenderSlary = ({slary}) => {
 const Slary = (props) => {
     const slary = props.staffs.map((slary) => {
         return (
-            <div key={slary.id}>
+            <div key={slary.id} className="col-12 col-md-6 col-lg-4 mt-2 mb-2">
                 <RenderSlary slary={slary} />
 
             </div>
