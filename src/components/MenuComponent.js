27debf4429e12e9dbb260c9374e020b5f3b69c1c
NguyenThanh <thnh.47@gmail.com>
2022-06-27 22:26:38 +0700

    first commit

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Card, CardImg, CardTitle } from 'reactstrap';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Card, CardImg, CardTitle } from "reactstrap";
 
-const RenderMenuItem = ({staff}) => {
-    return (
-        <Card className="border-dark text-center">
-        <Link to={`/nhanvien/${staff.id}`} >
-            <CardImg src={staff.image} atl={staff.name} />
-            <CardTitle className="text-center border-dark bg-info text-white">{staff.name}</CardTitle>
-        </Link>
-        </Card>
-    )
-}
-
-const Menu = (props) => {
-    const staffs = props.staffs.map((staff) => {
-        return (
-            <div key={staff.id} className="col-12 col-md-6 col-lg-2 mt-2 mb-2" >
-                <RenderMenuItem staff={staff} />
-            </div>
-        )
-    });
-    return (
-        <div className='container'>
-            <div className='row'>
-                <h2 className='ml-3 mt-3'>Nhân viên</h2>
-            </div>
-                <hr className=' mb-3'/>
-            <div className='row'>
-                {staffs}
-            </div>
-        </div>
-    );
+const RenderMenuItem = ({ staff }) => {
+  return (
+    <Card className="border-dark text-center">
+      <Link to={`/nhanvien/${staff.id}`}>
+        <CardImg src={staff.image} atl={staff.name} />
+        <CardTitle className="text-center border-dark bg-info text-white">
+          {staff.name}
+        </CardTitle>
+      </Link>
+    </Card>
+  );
 };
 
+class Menu extends Component {
+  render() {
+    const staffs = this.props.staffs.map((staff) => {
+      return (
+        <div key={staff.id} className="col-12 col-md-6 col-lg-2 mt-2 mb-2">
+          <RenderMenuItem staff={staff} />
+        </div>
+      );
+    });
+    return (
+      <div className="container">
+        <div className="row">
+          <h2 className="ml-3 mt-3">Nhân viên</h2>
+        </div>
+        <hr className=" mb-3" />
+        <div className="row">{staffs}</div>
+      </div>
+    );
+  }
+}
 export default Menu;
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,64 +1,66 @@
-import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardImg, CardImgOverlay, 
-    CardText, 
-    CardTitle } from 'reactstrap';
-import dateFormat from 'dateformat';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  CardBody,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardTitle,
+} from "reactstrap";
+import dateFormat from "dateformat";
+import { Link } from "react-router-dom";
 
-const RenderStaffDetail = ({staff}) => {
-    if (staff != null) {
+const RenderStaffDetail = ({ staff }) => {
+  if (staff != null) {
+    return (
+      <Card>
+        <CardBody className="row">
+          <div className="col-12 col-md-4 col-lg-3 mt-2 mb-2">
+            <CardImg width="100%" src={staff.image} alt={staff.name} />
+          </div>
+          <div className="col-12 col-md-8 col-lg-9 mt-2 mb-2">
+            <CardTitle>Họ và tên: {staff.name}</CardTitle>
+            <CardText>
+              Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
+            </CardText>
+            <CardText>
+              Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
+            </CardText>
+            <CardText>Phòng ban: {staff.department.name}</CardText>
+            <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+            <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+          </div>
+        </CardBody>
+      </Card>
+    );
+  } else {
+    return <div></div>;
+  }
+};
 
-        return (
-            <Card>
-                <CardBody className="row">
-                  <div className="col-12 col-md-4 col-lg-3 mt-2 mb-2">
-                    <CardImg width="100%" src={staff.image} alt={staff.name} />
-                  </div>
-                  <div className="col-12 col-md-8 col-lg-9 mt-2 mb-2">
-                    <CardTitle>Họ và tên: {staff.name}</CardTitle>
-                    <CardText>
-                      Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
-                    </CardText>
-                    <CardText>
-                      Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
-                    </CardText>
-                    <CardText>Phòng ban: {staff.department.name}</CardText>
-                    <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
-                    <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
-                  </div>
-                </CardBody>
-             </Card>
-        )
-    }
-    else {
-        return <div></div>
-    }
-}
-
-const  StaffList = (props) => {
-        if (props.staff != null) {
-
-            return (
-                <div className='container'>
-                    <div className='row'>
-                        <Breadcrumb className="ml-3">
-                            <BreadcrumbItem>
-                                <Link to='/nhanvien'>Nhân viên</Link>
-                            </BreadcrumbItem>
-                            <BreadcrumbItem active>{props.staff.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <hr />
-                    </div>
-                    <div>
-                        <RenderStaffDetail staff={props.staff} />
-                    </div>
-                </div>
-            )
-        }
-        else {
-            return <div></div>
-        }
-
-}
+const StaffList = (props) => {
+  if (props.staff != null) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb className="ml-3">
+            <BreadcrumbItem>
+              <Link to="/nhanvien">Nhân viên</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.staff.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <hr />
+        </div>
+        <div>
+          <RenderStaffDetail staff={props.staff} />
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+};
 
 export default StaffList;
